665082121f55f128adeb53ef6077153f70fa7fa7
jluode <126559954+jluode@users.noreply.github.com>
2023-05-25 14:09:33 +0300

    Uus enemy ja pari platformii

--- Assets/Jaakko testi/Scripts Jaakko/PlayerMovementJaakko.cs
+++ Assets/Jaakko testi/Scripts Jaakko/PlayerMovementJaakko.cs
@@ -4,7 +4,7 @@ public class PlayerMovementJaakko : MonoBehaviour
 {
     private float horizontal;
     [SerializeField] private float speed = 8f;
-    private float jumpingPower = 16f;
+    [SerializeField] private float jumpingPower = 16f;
     private bool isFacingRight = true;
 
     private Animator animator;
